be59a85db2b223fe83790de2ad96be3b784ecf99
Deepak Kumar <112487569+deepakkr028@users.noreply.github.com>
2024-01-27 22:08:11 +0530

    Update footer.php

--- include/footer.php
+++ include/footer.php
@@ -1,11 +1,10 @@
-
-      <footer class="footer_style">
+ <footer class="footer_style">
         <main class="container-fluid">
-         <div class="row p-3">
+        <div class="row p-3">
           <div class="col-md-4 col-12 offset-md-1"> 
-           <p class="mt-5 text-justify"> Developed by Deepak Kumar </p>
-           <p class="text-capitalize"> @copyright  All right reserved. </p>        
-   
+          <p class="mt-5 text-justify"> Developed by Deepak Kumar </p>
+          <p class="text-capitalize"> @copyright  All right reserved. </p>        
+
           </div>
         
           <div class="col-md-4 col-12">
@@ -26,7 +25,7 @@
           </div>
         
           <div class="col-md-2 col-12">
-           <h4>Links</h4>
+          <h4>Links</h4>
                       <ul>
                         <li><a href="index.php">HOME</a></li>
                         <li><a href="gallery.html">GALLERY</a></li>
@@ -35,18 +34,18 @@
                         <li><a href="aboutus.html">ABOUT US</a></li>
                       </ul>
           </div>
-         </div>
+        </div>
         
-         <div class="d-flex flex-row justify-content-center align-items-center flex-wrap">
-           <div class="p-md-5 p-2 flex-fill ">
-           <a href=""> <i class="fa fa-facebook-official" aria-hidden="true"></i> <span>Facebook</span> </a> </div>
+        <div class="d-flex flex-row justify-content-center align-items-center flex-wrap">
+          <div class="p-md-5 p-2 flex-fill ">
+          <a href=""> <i class="fa fa-facebook-official" aria-hidden="true"></i> <span>Facebook</span> </a> </div>
         
-           <div class="p-md-5 p-2 flex-fill "><a href="https://www.instagram.com/_deepak_kumar24/"><i class="fa fa-instagram" aria-hidden="true"></i><span>Instagram</span></a></div>
+          <div class="p-md-5 p-2 flex-fill "><a href="https://www.instagram.com/_deepak_kumar67/"><i class="fa fa-instagram" aria-hidden="true"></i><span>Instagram</span></a></div>
         
-           <div class="p-md-5 p-2 flex-fill "><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i><span>Twitter</span></a></div>
+          <div class="p-md-5 p-2 flex-fill "><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i><span>Twitter</span></a></div>
         
-           <div class="p-md-5 p-2 flex-fill "><a href="https://www.youtube.com/channel/UCrNQR5qU0y6Y2e8k4i4jwNQ"><i class="fa fa-youtube" aria-hidden="true"></i><span>Youtube</span></a></div>
+          <div class="p-md-5 p-2 flex-fill "><a href="https://www.youtube.com/channel/UCrNQR5qU0y6Y2e8k4i4jwNQ"><i class="fa fa-youtube" aria-hidden="true"></i><span>Youtube</span></a></div>
           
-         </div>
+        </div>
         </main>
       </footer>
